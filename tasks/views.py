05c0a166a29b1185036f33f93d3d7cfcb7a5a9b0
Shojib H. Ruhan <si.ruhan09@gmail.com>
2025-02-09 18:19:46 +0600

    responsive

--- tasks/views.py
+++ tasks/views.py
@@ -175,5 +175,36 @@ def search_event(request):
     
 
 def test(request):
-    participents= Participant.objects.all()
-    return render(request, "test.html", {"participents":participents})
+    type= request.GET.get('type', "upcoming_events")
+    # print(type)
+    
+    base_query= Event.objects.select_related('details').prefetch_related('participant')
+    if type == "upcoming_events":
+        events= base_query.filter(status= "U")
+    elif type == "past_events":
+        events= base_query.filter(status= "P")
+    elif type == "all":
+        events= base_query.all()
+
+    query= request.GET.get('q', " ")
+    # if query:
+    #     search= base_query.filter(name__icontains = query)
+    counts= Event.objects.aggregate(
+        total= Count('id'),
+        upcoming_events= Count('id', filter= Q(status= "U")),
+        past_events= Count('id', filter= Q(status= "P"))
+       
+    )
+    events= Event.objects.all()
+   
+    total_participant= Participant.objects.all().count()
+
+    context= {
+        "events": events,
+        "counts": counts,
+        "total_participant": total_participant
+    }
+
+
+    # return render(request, 'dashboard/manager-dashboard.html', context)
+    return render(request, "test.html", context)
